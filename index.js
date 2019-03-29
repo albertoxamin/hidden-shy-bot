ed9e09831f3da1c000e2e4e452a3ff47994a7cc0
Alberto Xamin <alberto@xamin.it>
2019-03-29 22:14:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const Telegraf = require('telegraf')
+var { Telegram, Markup } = require('telegraf')
+const crypto = require('crypto')
+
+var config
+
+config = {
+	token: '767345631:AAFnaUU7ZiXER-5pykfduhrrGrnBYmkvOOo'
+}
+
+const telegram = new Telegram(config.token, null)
+const bot = new Telegraf(config.token)
+
+const readKeyboard = (text) => {
+	return Markup.inlineKeyboard(
+		[Markup.callbackButton('Read', text)]
+	).extra()
+}
+
+
+bot.on('callback_query', (ctx) => {
+	let data = ctx.callbackQuery.data
+	ctx.answerCbQuery(data, true)
+})
+
+
+bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
+	let message = inlineQuery.query
+	let result = ['❓', '❗️', '❤️', '😏', '😅'].map(function (x) {
+		return {
+			type: 'article',
+			id: crypto.createHash('md5').update(message + x).digest('hex'),
+			title: x,
+			input_message_content: {
+				message_text: message.split('').map(char => x).join('').toString(),
+				parse_mode: 'Markdown'
+			},
+			reply_markup: Markup.inlineKeyboard(
+				[Markup.callbackButton('Read', message)]
+			)
+		}
+	})
+	return answerInlineQuery(result)
+})
+
+bot.catch((err) => {
+	console.log('Ooops', err)
+})
+
+bot.startPolling()
