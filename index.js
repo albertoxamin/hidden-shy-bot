4dad81818f1cabb02b96ec93b717fa6410656cdc
Alberto Xamin <alberto@xamin.it>
2019-06-05 11:05:39 +0200

    now the bot is serverless with web-hooks

--- index.js
+++ index.js
@@ -71,5 +71,18 @@ bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
 
 bot.catch((err) => console.log('Ooops', err))
 
-const start = () => bot.startPolling(30, 100, null, start)
-start()
+const { json } = require('micro')
+
+module.exports = async function (req, res) {
+	try {
+		const body = await json(req)
+  		bot.handleUpdate(body)
+		res.statusCode = 200
+		res.end('')
+	} catch (e) {
+		res.statusCode = 500
+		res.setHeader('Content-Type', 'text/html')
+		res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>')
+		console.error(e.message)
+	}
+};
